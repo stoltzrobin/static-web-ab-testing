31e27e4ff3d4966c96b1998b3486293e4f2f5132
stoltzrobin <robinstoltz90@gmail.com>
2020-01-14 16:25:01 +0100

    Added jest the right way for gatsby 😅

--- src/Experiment/__tests__/ServerExperimentHelper.test.js
+++ src/Experiment/__tests__/ServerExperimentHelper.test.js
@@ -4,5 +4,8 @@ describe("ServerExperimentHelper", () => {
   it("Should have 50% of test case 1", () => {
     console.log("S: ", setExperimentGroup("123", null, [0.5], 0.7))
     expect(true).toBe(true)
-  });
+  })
+  it("Should run variant 1", () => {
+    expect(true).toBe(true)
+  })
 })
